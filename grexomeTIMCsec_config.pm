07004e83f0b301b584c1ecc156ad6ee4322ea195
manojmw <manojmonmax@gmail.com>
2022-03-31 11:51:31 +0200

    modifying CACHE path

--- grexomeTIMCsec_config.pm
+++ grexomeTIMCsec_config.pm
@@ -56,8 +56,7 @@ sub refGenome {
 sub vepCacheFile {
     # you'll need to customize the array of possible values for $cachedir,
     # we use the first dir that exists (works on luxor and fauve)
-    foreach my $cachedir ("/data/nthierry/PierreRay/RunSecondaryAnalyses/",
-			  "/home/nthierry/sshMounts/luxor/data/nthierry/PierreRay/RunSecondaryAnalyses/") {
+    foreach my $cachedir ("/data/waglem/") {
 	if (-d $cachedir) {
 	    # the name of the file itself, can probably stay as-is
 	    my $cacheFile = "VEP_cache";
